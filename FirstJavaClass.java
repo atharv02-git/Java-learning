9a51b1584353a6e6a97c29783dfa47c824b6430a
Atharv <atharvbhandare2001@gmail.com>
2022-08-20 23:45:05 +0530

    java basic concepts

---
+++ FirstJavaClass.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class FirstJavaClass {
+    public static void main(String[] args) {
+        // int firstNumber = 10;
+        // int secondNumber = 5;
+
+        // int addition = firstNumber + secondNumber;
+        // double doubledDiv = (double)firstNumber / (double)secondNumber;
+        // int moduloDiv = firstNumber % secondNumber;
+
+        // System.out.println("Addition is: "+addition);
+        // System.out.println("Doubled division is: "+doubledDiv);
+        // System.out.println("Modulo division is: "+moduloDiv); //gives Remainder
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your name: ");
+        // String str = sc.nextLine();
+        // System.out.println("Your Name is: ");
+        // System.out.println(str);
+        // sc.close();
+
+        // Switch Case
+
+        // char grade = 'A';
+
+        // switch (grade) {
+        // case 'A':
+        // System.out.println("Very good grade");
+        // break;
+        // case 'B':
+        // System.out.println("B grade");
+        // default:
+        // System.out.println("Wrong Argument");
+        // break;
+        // }
+
+        /* int a = 1;
+        while (true) {
+            System.out.println(a);
+            a++;
+            if (a > 11) {
+                break;
+            }
+        } */
+        int a = 1;
+        while(a < 100){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
