97c5ab338864d2792f25629dcb3812fe9e76701d
Atharv <atharvbhandare2001@gmail.com>
2022-08-21 17:02:14 +0530

    Accepting user input in the form of array

--- FirstJavaClass.java
+++ FirstJavaClass.java
@@ -43,10 +43,42 @@ public class FirstJavaClass {
                 break;
             }
         } */
-        int a = 1;
-        while(a < 100){
-            System.out.println(a);
-            a++;
+        // int a = 1;
+        // while(a < 100){
+        //     System.out.println(a);
+        //     a++;
+        // }
+
+        // Arrays
+        // int marks[] = {10, 30, 34, 48, 12, 100, 56};
+        // for(int i = 0; i < marks.length; i++) {
+        //     System.out.println("Array of marks is: " + marks[i]);
+        // }
+
+        // Accepting user input in the form of Array
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        // Reads the size of the array
+        n = sc.nextInt();
+
+        //creates an array in the memory of length 10
+        int userArray[] = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < n; i++) {
+            //reading array elements from the user  
+            userArray[i] = sc.nextInt();
         }
+
+        System.out.println("Array elements are: ");
+        // Accessing the elements of the array using for loop
+        for(int i = 0; i < n; i++) {
+            System.out.println(userArray[i]);
+        }
+
+        sc.close();
     }
+
 }
